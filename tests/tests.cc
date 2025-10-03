c524ae36cb3d3e9acaeb97a0d0362b56f185de7d
Yiyu Chen <toyiyu1030@gmail.com>
2025-10-03 18:49:04 +0000

    delete one

--- tests/tests.cc
+++ tests/tests.cc
@@ -75,17 +75,6 @@ TEST_CASE("Example: Print Prompt Ledger", "[ex-3]") {
   REQUIRE(CompareFiles("./ex-1.txt", "./prompt.txt"));
 }
 
-TEST_CASE("Example: Simple Deposit", "[ex-4]") {
-  Atm atm;
-  atm.RegisterAccount(1234, 123, "Leon Chen", 300.30);
-  atm.DepositCash(1234, 123, 20.00);
-  auto accounts = atm.GetAccounts();
-
-  Account leom_account = accounts[{1234, 123}];
-
-  REQUIRE(leom_account.balance == 320.30);
-}
-
 TEST_CASE("Example: Repeat Register", "[ex-5]") {
   Atm atm;
   atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
