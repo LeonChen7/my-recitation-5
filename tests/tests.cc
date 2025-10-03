cee28c39883425c7cf05dde6b3e928cd8c7949ec
Yiyu Chen <toyiyu1030@gmail.com>
2025-10-03 20:40:09 +0000

    all

--- tests/tests.cc
+++ tests/tests.cc
@@ -84,3 +84,37 @@ TEST_CASE("Example : Repeat", "[ex-4]") {
   auto accounts = atm.GetAccounts();
   REQUIRE(accounts.size() == 1);
 }
+
+TEST_CASE("Example : Negative Withdraw", "[ex-5]") {
+  Atm atm;
+  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+  REQUIRE_THROWS(atm.WithdrawCash(12345678, 1234, -20));
+  auto accounts = atm.GetAccounts();
+  Account sam_account = accounts[{12345678, 1234}];
+
+  REQUIRE(sam_account.balance == 300.30);
+}
+
+TEST_CASE("Example: Print Prompt Ledger", "[ex-6]") {
+  Atm atm;
+  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+  auto& transactions = atm.GetTransactions();
+  transactions[{12345678, 1234}].push_back(
+      "Withdrawal - Amount: $200.40, Updated Balance: $99.90");
+  transactions[{12345678, 1234}].push_back(
+      "Deposit - Amount: $40000.00, Updated Balance: $40099.90");
+  transactions[{12345678, 1234}].push_back(
+      "Deposit - Amount: $32000.00, Updated Balance: $72099.90");
+  atm.PrintLedger("./prompt.txt", 12345678, 1234);
+  REQUIRE(CompareFiles("./ex-1.txt", "./prompt.txt"));
+  REQUIRE_THROWS(atm.PrintLedger("./prompt.txt", 1234567, 123));
+}
+
+TEST_CASE("Example : Simple Diposit", "[ex-7]") {
+  Atm atm;
+  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+  atm.DepositCash(12345678, 1234, 20.00);
+  auto accounts = atm.GetAccounts();
+  Account sam_account = accounts[{12345678, 1234}];
+  REQUIRE(sam_account.balance == 320.30);
+}
