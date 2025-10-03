e72a40a6be2767060e2d85e40adbddbcb92578be
Yiyu Chen <toyiyu1030@gmail.com>
2025-10-03 20:29:15 +0000

    exception

--- tests/tests.cc
+++ tests/tests.cc
@@ -75,12 +75,12 @@ TEST_CASE("Example: Print Prompt Ledger", "[ex-3]") {
   REQUIRE(CompareFiles("./ex-1.txt", "./prompt.txt"));
 }
 
-TEST_CASE("Example : Negative Withdraw", "[ex-4]") {
+TEST_CASE("Example : Repeat", "[ex-4]") {
   Atm atm;
   atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
-  atm.WithdrawCash(12345678, 1234, -20);
-  auto accounts = atm.GetAccounts();
-  Account sam_account = accounts[{12345678, 1234}];
 
-  REQUIRE(sam_account.balance == 320.30);
+  REQUIRE_THROWS(atm.RegisterAccount(12345678, 1234, "Leon Chen", 400.30));
+
+  auto accounts = atm.GetAccounts();
+  REQUIRE(accounts.size() == 1);
 }
